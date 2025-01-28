31afb49c7372a9f830fec59212f7b5661ffef9aa
Anjalinegi <anjalinegi26500@gmail.com>
2025-01-29 01:32:03 +0530

    Update ArithmeticCalc.java

--- ArithmeticCalc.java
+++ ArithmeticCalc.java
@@ -1,30 +1,46 @@
-import java.util.Scanner;
-
+import java.util.Scanner; 
 public class ArithmeticCalc {
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+
+        // Taking the first digit as input
         System.out.println("Enter the first digit:");
-        Integer a = sc.nextInt();
-        System.out.println("Enter an operator(+,-,*,/,%):");
-        char opr = sc.next().charAt(0);
+        Integer a = scanner.nextInt();
+
+        // Taking the operator as input
+        System.out.println("Enter an operator (+, -, *, /, %):");
+        char operator = scanner.next().charAt(0);
+
+        // Taking the second digit as input
         System.out.println("Enter the second digit:");
-        Integer b = sc.nextInt();
-        switch (opr) {
-        case '+' -> {
-            System.out.println("a+b=" + a+b);
+        Integer b = scanner.nextInt();
+
+        // Switch case to perform the operation based on the operator
+        switch (operator) {
+            case '+' -> {
+                // Performing addition
+                System.out.println("a + b = " + (a + b));
             }
-        case '-' -> {
-            System.out.println("a-b=" + (a-b));
-            }   
-        case '*' -> {
-            System.out.println("a*b=" + a*b);   
-            }  
-        case '/'  ->
-            System.out.println("a/b=" + a/b);
-        case '%' -> {
-            System.out.println("a*b=" + a%b);   
-            } 
-        default -> System.out.println("invalid operator!");
+            case '-' -> {
+                // Performing subtraction
+                System.out.println("a - b = " + (a - b));
+            }
+            case '*' -> {
+                // Performing multiplication
+                System.out.println("a * b = " + (a * b));
+            }
+            case '/' -> {
+                // Performing division
+                System.out.println("a / b = " + (a / b));
+            }
+            case '%' -> {
+                // Performing modulo operation
+                System.out.println("a % b = " + (a % b));
+            }
+            default -> System.out.println("Invalid operator!");  // Handling invalid operator
         }
-}
+
+        scanner.close();  // Closing the scanner to release resources
+    }
 }
